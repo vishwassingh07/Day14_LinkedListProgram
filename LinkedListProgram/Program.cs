8350886b38ad81875070e562d522729ed08a9114
Vishwas <baghel.vishwas0@gmail.com>
2022-06-20 15:53:49 +0530

    [Vishwas]add: Linked List Program Added

---
+++ LinkedListProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LinedListProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linked List Program \n - - - - - - - - - - - - - - - - - ");
+        }
+    }
+}
