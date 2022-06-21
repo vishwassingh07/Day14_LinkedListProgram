7b10c150d760da7248cc94fc64c57f185247f67d
Vishwas <baghel.vishwas0@gmail.com>
2022-06-21 13:42:26 +0530

    [Vishwas]add: Create Simple Linked List

---
+++ LinkedListProgram/LinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    public class LinkedList
+    {
+        public Node Head;
+        public Node Tail;
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+        public void AddNode(Node node)
+        {
+            if (Head == null & Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+            {
+                Console.WriteLine("The Linked List is empty ");
+            }
+            else
+            {
+                Console.WriteLine("The Linked List : ");
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    if (temp.next != null)
+                    {
+                        Console.Write(" --> ");
+                    }
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- LinkedListProgram/Program.cs
+++ LinkedListProgram/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-namespace LinedListProgram
+
+namespace LinkedListProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linked List Program \n - - - - - - - - - - - - - - - - - ");
+            LinkedList linkedlist = new LinkedList();
+            Node node1 = new Node(70);
+            Node node2 = new Node(30);
+            Node node3 = new Node(56);
+            linkedlist.AddNode(node1);
+            linkedlist.AddNode(node2);
+            linkedlist.AddNode(node3);
+            linkedlist.Display();
         }
     }
 }
